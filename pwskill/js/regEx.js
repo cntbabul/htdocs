325b8671b75030af6e5bf1fd35ffd20b7c747a1e
cntbabul <cntbabul@gmail.com>
2023-03-13 19:48:28 +0530

    regEx

--- pwskill/js/regEx.js
+++ pwskill/js/regEx.js
@@ -12,3 +12,13 @@ const strToCheck  = "pw is growing at a rapid speed and recently they are workin
 
 const result = regExThree.test(strToCheck)
 console.log(result);
+const anotherResult = strToCheck.match(regExThree)
+console.log(anotherResult);
+
+const OneMoreResult = strToCheck.replace(regExThree, 'p-w')
+console.log(OneMoreResult);
+
+const webUrl = "https:pwskills.com/hitesh%20choudhary"
+
+const useableUrl = webUrl.replace(/%20/, '-')
+console.log(useableUrl);
