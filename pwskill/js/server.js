7402bca33ac8b799e6937cf7d116e012c85901cf
cntbabul <cntbabul@gmail.com>
2023-03-23 09:00:53 +0530

    js

---
+++ pwskill/js/server.js
@@ -0,0 +1,30 @@
+// let myHeros = ["thor", "spiderman"]
+// let dcHeros = ["batman", "flash", "superman"]
+
+// let heroPower = {
+//     thor: "hammer",
+//     spiderman: "sling",
+
+//     getspidermanPower: function(){
+//         console.log(`Spidy power is ${this.spidermsn}`);
+//     }
+// }
+
+// Object.prototype.hitesh = function(){
+//     console.log(`hitesh is present in all objects`);
+// }
+
+// console.log(myHeros.hitesh());
+// console.log(heroPower.hitesh());
+
+
+//22nd revision Live class
+// //conditionals
+// if(condition){
+//     statement
+// } 
+
+
+// let userToken = undefined
+// let googleAuthtoken = 
+
