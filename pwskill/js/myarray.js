c8ae8695bb49ede17c238527f4b0ea0b3230988f
cntbabul <cntbabul@gmail.com>
2023-03-23 09:08:44 +0530

    js

---
+++ pwskill/js/myarray.js
@@ -0,0 +1,5 @@
+const oneArray = [1, 2, 3, 4]
+const twoArray = [5, 6, 7, 8]
+
+const threeArray = [...oneArray, ...twoArray]
+console.log(threeArray);
---
+++ pwskill/js/setandMap.js
@@ -0,0 +1,10 @@
+let emptySet = new Set()
+
+console.log(emptySet.size)
+
+let myArray = [1, 2, 3, 4, 3, 2]
+let newSet new Set([...myArray])
+
+newSet.add(9)
+console.clear()
+console.log(newSet);
