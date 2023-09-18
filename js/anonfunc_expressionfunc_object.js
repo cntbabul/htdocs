06791861404f1c2b0817caa35df82ce331969f74
MD BABUL MIAH <56152131+cntbabul@users.noreply.github.com>
2023-09-18 03:01:21 +0000

    js

--- js/anonfunc_expressionfunc_object.js
+++ js/anonfunc_expressionfunc_object.js
@@ -19,3 +19,19 @@ x();
 //============================================================================================================================
 let obj = {id:101, name:"sabana", salary:20000};
 console.log(obj);
+//====================================================
+let emp = new Object();
+emp.id = 102;
+emp.name = "sami";
+emp.salary = 25000;
+console.log(emp);
+console.log(obj);
+//==========================================
+function Emp(i, n, s){
+    this.id = i;
+    this.name = n;
+    this.salary =s;
+}
+const e = new Emp(103, "sunu", 26000);
+console.log(e);
+console.log(Object.ids);
