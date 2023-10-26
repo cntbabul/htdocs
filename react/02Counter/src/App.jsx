364f40b4a9ce661942a24265226c2ceab8c7b2d6
babul <cntbabul@gmail.com>
2023-10-26 06:57:29 +0530

    vite

---
+++ react/02Counter/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+let Counter = 5
+function App() {  return (
+    <>
+    <h1>chai aur code</h1>
+    <h2>hello babul</h2>
+
+   <h2>Counter Value: {Counter}</h2>
+    <button>Add Value</button>
+    <button>Substract Value</button>
+    </>
+  )
+}
+
+export default App
