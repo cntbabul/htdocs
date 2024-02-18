9f7da1d4fed92ae24b37d94028ae1b654dc45517
chromebook <cntbabul@gmail.com>
2024-02-18 11:09:51 +0530

    18-02-2024

--- pwskill/js/promise.js
+++ pwskill/js/promise.js
@@ -12,20 +12,20 @@ const promiseOne = new Promise(function(resolve, reject)
 
 })
 
-//consuming apromise
+//consuming a promise
 promiseOne.then(function(){
     console.log(`Promise is resolved`);
 }).catch(function(){
     console.log(`Promise is rejected`);
 })
 
-new Promise(function(resolve, reject){
+ new Promise(function(resolve, reject){
     setTimeout(function(){
         console.log(`Async task 2`);
         resolve();
     }, 1000)
 }).then(function(){
-    console.log(`Promise consumed: 2`)
+    // console.log(`Promise consumed: 2`)
 })
 
 const promiseThree = new Promise(function(resolve,reject){
@@ -41,3 +41,25 @@ promiseThree.then(function(user){
 }).catch(function(error){
    console.log("Error in getting user details");
 });
+
+// promiseFour
+// .then((user => {console.log(user)}))
+// .catch((error) => {
+//     console.log(error)
+// })
+
+
+async function getAllUsers(){
+ const response = await fetch ('http://jsonplaceholder.typicode.com/users') 
+ const data = response.json()
+ console.log(data)
+}
+
+getAllUsers()
+
+fetch('http://jsonplaceholder.typicode.com/users')
+.then (function(response){
+    response.json()
+})
+.then((data) => console.log(data))
+.catch((error) => {console.log(error)})
