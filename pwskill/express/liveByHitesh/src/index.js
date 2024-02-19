6bf98a74a7063bebbaf37d76839f491581857c26
chromebook <cntbabul@gmail.com>
2024-02-19 16:28:24 +0530

    Commit

--- pwskill/express/liveByHitesh/src/index.js
+++ pwskill/express/liveByHitesh/src/index.js
@@ -1,8 +1,42 @@
 
 import app from "./app.js";
-const port = 3000
+import mongoose from "mongoose";
+const port = 3000;
 
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+// mongoose.connect('mongodb://127.0.0.1:27017/test')
+//   .then(() => console.log('Connected!'));
+
+( async () => {
+  try {
+    //DB connection
+    await mongoose.connect('dbstring')
+    console.log("DB connected successfully")
+
+    app.on("error", (err) => {
+      console.log("ERROR: ", err)
+      throw err
+    })
+
+    const onListening = () =>{
+      console.log(`Listening on port ${port}`)
+    }
+
+
+    app.listen(port, onListening)
+
+  } catch (error) {
+    console.error("ERROR : ", err)
+    throw err
+  }
+})()
+
+
+
+
+
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
