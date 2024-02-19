ce2846831937be690013ec314ba6350ba5346eba
chromebook <cntbabul@gmail.com>
2024-02-19 11:20:09 +0530

    commit

--- pwskill/express/liveByHitesh/src/index.js
+++ pwskill/express/liveByHitesh/src/index.js
@@ -2,9 +2,6 @@
 import app from "./app.js";
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
