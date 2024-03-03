08cb35125b84b94562369fd4105778e0bed801bd
chromebook <cntbabul@gmail.com>
2024-03-03 11:23:00 +0530

    03-03-2023

---
+++ pwskill/vite/vite-project/src/components/card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function card() {
+  return (
+    <div>
+        <h1>Card</h1>
+        <h1>Email</h1>
+        <h1>Photo</h1>
+        <h1>Contact</h1>    
+    </div>
+
+
+)}
+
+export default card
---
+++ pwskill/vite/vite-project/src/components/myH.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function myH() {
+  return (
+    <div>My new Component</div>
+  )
+}
+
+export default myH
