273c0422cbe589e3334f86c1962d64aa0f11eb4e
MD BABUL MIAH <56152131+cntbabul@users.noreply.github.com>
2024-08-10 13:35:52 +0530

    Delete bloodBankFinal directory

--- bloodBankFinal/controllers/testController.js
+++
@@ -1,8 +0,0 @@
-const testController = (req, res) => {
-  res.status(200).send({
-    message: "Welcome user",
-    success: true,
-  });
-};
-
-module.exports = { testController };
--- bloodBankFinal/env.js
+++
@@ -1,2 +0,0 @@
-// PORT=8080
-// MONGO_URL=mongodb+srv://babul:123@cluster0.i1wta2y.mongodb.net/bloodBankFinal
